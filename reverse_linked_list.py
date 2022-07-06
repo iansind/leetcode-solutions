a4ab104358bcdac5b116649721a80e3467a7511f
iansind <108632228+iansind@users.noreply.github.com>
2022-07-05 22:20:38 -0500

    Create reverse_linked_list.py

---
+++ reverse_linked_list.py
@@ -0,0 +1,26 @@
+# Problem found at: https://leetcode.com/problems/reverse-linked-list/submissions/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        previous_node = None
+        current_node = head
+        
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            
+        head = previous_node
+            
+        return head
+        
