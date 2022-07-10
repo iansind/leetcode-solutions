02272a75d25c13ceba96a0c1e2e6b15c1819d9de
iansind <108632228+iansind@users.noreply.github.com>
2022-07-10 17:07:51 -0500

    Create palindrome_number.py

---
+++ palindrome_number.py
@@ -0,0 +1,11 @@
+# Problem found at: https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        return x == x[::-1]
+        
