8c8feef1ee1bab00da485e3d93bb536ac1f4f30f
iansind <108632228+iansind@users.noreply.github.com>
2022-11-27 14:07:49 -0600

    Create running_sum_1d.py

---
+++ running_sum_1d.py
@@ -0,0 +1,13 @@
+# Problem found at https://leetcode.com/problems/running-sum-of-1d-array/submissions/
+
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        out = []
+        for x in range(len(nums)):
+            out.append(sum(nums[0:x+1]))
+            
+        return out
