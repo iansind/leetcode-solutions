a246fbe2e7e25d2d3f26698143c7f6e10d25e502
iansind <108632228+iansind@users.noreply.github.com>
2022-11-27 21:56:08 -0600

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+# Problem found at https://leetcode.com/problems/reverse-string/
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        l = len(s)
+        
+        for x in range(int(len(s)/2)):
+            p1, p2 = s[x], s[l-x-1]
+            s[x], s[l-x-1] = p2, p1
+            
